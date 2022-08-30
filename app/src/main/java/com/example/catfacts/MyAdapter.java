6cd33f96e479168daa5baa66b9f2a0e58029df9e
Artur <arturstasicki@gmail.com>
2022-08-30 10:44:26 +0200

    Simple adapter

---
+++ app/src/main/java/com/example/catfacts/MyAdapter.java
@@ -0,0 +1,45 @@
+package com.example.catfacts;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHoler>{
+    @NonNull
+    @Override
+    public MyViewHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fact_row, parent, false);
+        return new MyViewHoler(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHoler holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+}
+
+class MyViewHoler extends RecyclerView.ViewHolder{
+
+    private TextView text_fact;
+    private TextView text_factLength;
+    public MyViewHoler(@NonNull View itemView) {
+        super(itemView);
+        text_fact = itemView.findViewById(R.id.facts);
+        text_factLength = itemView.findViewById(R.id.factsLendth);
+    }
+    public TextView get_fact(){
+        return text_fact;
+    }
+    public TextView get_factLength(){
+        return text_factLength;
+    }
+}
